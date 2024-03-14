a574bc6ef7a44556bb15a547975da87a3505443b
Mukund Sutar <87423576+mukundsutar@users.noreply.github.com>
2024-03-14 22:26:22 +0530

    Changes: 1. Testing Link to new pages

--- src/PastedData.jsx
+++ src/PastedData.jsx
@@ -5,44 +5,7 @@ import { Textarea } from "@chakra-ui/react";
 import { Center } from "@chakra-ui/react";
 
 export default function PastedData() {
-    const [decryptedData, setDecryptedData] = useState();
-
-    let location = useLocation();
-    // console.log(location.key);
-    const encryptedData = location.pathname.slice(1);
-
-    useEffect(() => {
-        const handleSubmit = async () => {
-            try {
-                const response = await axios.post(
-                    "http://localhost:3001/api/submit-encrypted-form",
-                    { encryptedData }
-                );
-                // console.log(response.data);
-                
-            } catch (error) {
-                console.error("Error submitting form:", error);
-            }
-        };
-
-        handleSubmit();
-    }, []);
-
-    useEffect(() => {
-        const handleGetDecryptedData = async () => {
-            try {
-                const response = await axios.get(
-                    "http://localhost:3001/api/get-decrypted-data"
-                );
-                console.log("Response data:", response.data); 
-                setDecryptedData(JSON.stringify(response.data.decryptedData));
-            } catch (error) {
-                console.error("Error getting encrypted data:", error);
-            }
-        };
-
-        handleGetDecryptedData();
-    }, []); // Run once when the component mounts
+    
 
     return (
         <>
--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ const router = createBrowserRouter([
         element: <App />,
     },
     {
-        path: "/:encryptedData",
+        path: "/:id",
         element: <PastedData />,
     },
 ]);
